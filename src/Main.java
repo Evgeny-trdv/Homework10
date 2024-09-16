2f34000db0d7b9073efd3aa78e860ff77c8f6322
evgen <evgeny15q@gmail.com>
2024-09-16 18:20:09 +0700

    Homework 10 start

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task one");
-        String firstName = "Ivan";
-        String middleName = "Ivanov";
-        String lastName = "Ivanovich";
-        String fullName = middleName + " " + firstName + " " + lastName;
-        System.out.println("Ф. И. О. сотрудника - " + fullName);
 
-        System.out.println("Task two");
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
-
-        System.out.println("Task three");
-        String fullNameSecond = "Иванов Семён Семёнович";
-        System.out.println("Данные Ф. И. О. сотрудника - " + fullNameSecond.replace('ё', 'е'));
-    }
+         }
 }
